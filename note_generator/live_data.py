3539ea5b58461c8c39d21ac0ece06e91fd987038
segfault <segfaul@pop-os.localdomain>
2023-06-03 16:51:18 +0300

    added node gen

---
+++ note_generator/live_data.py
@@ -0,0 +1,31 @@
+import numpy as np
+import sounddevice as sd
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+
+# Define the duration of audio to record in seconds
+duration = 10.0  # for example, 2 seconds
+# Define the sample rate
+sample_rate = 44100  # typical value for audio processing
+
+sd.default.device = 11
+
+# Record audio for 'duration' seconds
+audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+
+# Wait for the recording to finish
+sd.wait()
+
+# Since the audio is recorded as 2D array, we take the first dimension to get 1D audio data
+audio = audio[:, 0]
+
+# Compute the spectrogram
+D = librosa.amplitude_to_db(np.abs(librosa.stft(audio)), ref=np.max)
+
+# Display the spectrogram
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(D, sr=sample_rate, x_axis='time', y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Spectrogram')
+plt.show()
---
+++ note_generator/test.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.io.wavfile import write
+
+# Define constants
+sample_rate = 44100  # standard audio sample rate
+duration = 5.0  # duration of each note in seconds
+
+# Define frequencies for guitar strings in standard tuning
+# Using equal temperament tuning formula
+guitar_frequencies = [(2**((n-49)/12))*440 for n in range(28, 40)] 
+guitar_notes = ['E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3', 'E3']
+
+# Generate each note and save to .wav file
+for freq, note in zip(guitar_frequencies, guitar_notes):
+    # Generate samples for the sine wave
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+    note_samples = 0.5 * np.sin(2 * np.pi * freq * t)
+
+    # Ensure that highest value is in 16-bit range
+    audio = note_samples * (2**15 - 1) / np.max(np.abs(note_samples))
+    audio = audio.astype(np.int16)
+
+    # Write .wav file for the note
+    write(f'{note}.wav', sample_rate, audio)
